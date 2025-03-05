a2f859fc72117738c39b56683218b1cf86fdc5f0
Ice129 <91084504+Ice129@users.noreply.github.com>
2025-03-05 18:29:51 +0000

    Refactor startup script: move to Implementation directory, enhance zip extraction, and update desktop shortcut creation

--- Setup/setup.py
+++ Setup/setup.py
@@ -1,9 +1,9 @@
 import os
 import shutil
-# import requests
 import zipfile
 import subprocess
 import winreg
+import sys
 
 user_path = os.path.expanduser("~")
 new_folder = os.path.join(user_path, "Database Viewer Team 3")
@@ -13,37 +13,50 @@ print(f"Created folder: {new_folder}")
 
 cwd = os.getcwd()
 
-# unzip WW1-Project-Startup.zip
-zip_file = os.path.join(cwd, "WW1-Project-Startup.zip")
+# unzip WW1-Project-Startup.zip to current working directory
+zip_file = None
+for file in os.listdir(cwd):
+    if file.startswith("WW1-Project-") and file.endswith(".zip"):
+        zip_file = os.path.join(cwd, file)
+        zip_name = file[:-4]
+        break
+if zip_file is None:
+    raise FileNotFoundError(f"Could not find WW1-Project-*.zip in {cwd}")
+print(f"Found WW1-Project-Startup.zip in {cwd}")
+
 with zipfile.ZipFile(zip_file, "r") as zip_ref:
-    zip_ref.extractall(new_folder)
-    old_folder = new_folder
-    new_folder = os.path.join(new_folder, "WW1-Project-Startup")
-print("Unzipped WW1-Project-Startup.zip")
+    zip_ref.extractall(cwd)
+print("Unzipped WW1-Project-Startup.zip to current directory")
 
+# Move contents of Implementation directory to new folder
+implementation_dir = os.path.join(cwd, zip_name, "Implementation")
+if os.path.exists(implementation_dir):
+    for item in os.listdir(implementation_dir):
+        src = os.path.join(implementation_dir, item)
+        dst = os.path.join(new_folder, item)
+        if os.path.isdir(src):
+            shutil.copytree(src, dst, dirs_exist_ok=True)
+            print(f"Moved {src} to {dst}")
+        else:
+            shutil.copy2(src, dst)
+            print(f"Moved {src} to {dst}")
+
+
+# Clean up
 os.remove(zip_file)
-files = os.listdir(new_folder)
-for file in files:
-    if file != "Implementation":
-        shutil.rmtree(os.path.join(new_folder, file), ignore_errors=True)
-
-print("Moved files to new folder")
-
-shutil.move(os.path.join(new_folder, "Implementation"), old_folder)
-shutil.rmtree(new_folder)
-new_folder = old_folder
-
-shutil.move("startup.py", new_folder)
-
-print("Moved startup.pyw to new folder")
+shutil.rmtree(os.path.join(cwd, zip_name), ignore_errors=True)
+print("Moved Implementation contents to new folder and cleaned up")
 
 with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders") as key:
     desktop_dir, _ = winreg.QueryValueEx(key, "Desktop")
     desktop_dir = os.path.expandvars(desktop_dir)
 
 with open(os.path.join(desktop_dir, "Start Database Viewer.bat"), "w") as f:
-    f.write(f'@echo off\nstart python "{os.path.join(new_folder, "startup.pyw")}"')
-    
+    batch_content = f"""@echo off
+start python "{os.path.join(new_folder, "startup.pyw")}"
+"""
+    f.write(batch_content)
+
 print("Created Start Database Viewer.bat on desktop")
 
-# subprocess.Popen(["python", os.path.join(new_folder, "startup.py")])
+subprocess.Popen([sys.executable, os.path.join(new_folder, "startup.py")])
