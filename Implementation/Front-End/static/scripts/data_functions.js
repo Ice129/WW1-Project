88309d8d140ee93f9be6b03ce277ba0f3c763c86
egisac <143981758+egisac@users.noreply.github.com>
2025-03-07 01:08:27 +0000

    Update data_functions.js

---
+++ Implementation/Front-End/static/scripts/data_functions.js
@@ -0,0 +1,32 @@
+function fetchData(forename, surname, regiment, databaseName) {
+    const url = '/get_data';
+    const body = {
+        databaseName: databaseName,
+        forename: forename,
+        surname: surname,
+        regiment: regiment
+    };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'  // Ensure the content type is JSON
+        },
+        body: JSON.stringify(body)  // Convert the body to a JSON string
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(err => {
+                console.error('Validation Error:', err);
+                throw new Error('Validation Error');
+            });
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
