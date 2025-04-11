d3f03ce55fdc78e4c50fa8e8b54f401f49b2ddb3
Edward Quail <edward.q1@virginmedia.com>
2025-04-11 09:57:36 +0100

    Unit testing Xcel Dowloader

--- Implementation/Back-End/testing_DL.py
+++ Implementation/Back-End/testing_DL.py
@@ -49,14 +49,33 @@ class MyTestCase(unittest.TestCase):
         Headers = [description[0] for description in cursor.description]
         self.assertEqual(Headers[-1], "photo_incl")  #The last Header should be photo_incl
 
+    def test_Data_Base_data_Extraction_1(self):
+        database = sqlite3.connect("database.db")
+        cursor = database.cursor()
+        cursor.execute("SELECT * FROM NewspaperReferences2025")
+        Data = cursor.fetchall()
+        self.assertEqual(Data[0][1],  "Abbertson")  #The first Surname should be Abberson
+
     def test_Data_Base_data_Extraction_2(self):
         database = sqlite3.connect("database.db")
         cursor = database.cursor()
         cursor.execute("SELECT * FROM NewspaperReferences2025")
         Data = cursor.fetchall()
-        Headers = [description[0] for description in cursor.description]
-        self.assertEqual(Headers[-1], "photo_incl")  #The last Header should be photo_incl
+        self.assertEqual(Data[0][2],  "W")  #The first forename should be W
 
+    def test_Data_Base_data_Extraction_last_1(self):
+        database = sqlite3.connect("database.db")
+        cursor = database.cursor()
+        cursor.execute("SELECT * FROM NewspaperReferences2025")
+        Data = cursor.fetchall()
+        self.assertEqual(Data[-1][1],  "Yates")  #The Surname of the last data entry should be Yates
+
+    def test_Data_Base_data_Extraction_last_0(self):
+        database = sqlite3.connect("database.db")
+        cursor = database.cursor()
+        cursor.execute("SELECT * FROM NewspaperReferences2025")
+        Data = cursor.fetchall()
+        self.assertEqual(Data[-1][0],  3629)  #the last data entry should be the 3629th value
 
 if __name__ == '__main__':
     unittest.main()
