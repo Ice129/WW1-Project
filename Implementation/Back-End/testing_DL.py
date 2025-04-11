ba7285bce8beaccf56f5ae339cf15c32bb7491f6
Edward Quail <edward.q1@virginmedia.com>
2025-04-11 09:59:40 +0100

    Unit testing Xcel Dowloader

--- Implementation/Back-End/testing_DL.py
+++ Implementation/Back-End/testing_DL.py
@@ -16,22 +16,6 @@ class MyTestCase(unittest.TestCase):
         Headers = [description[0] for description in cursor.description]
         self.assertEqual(Headers[0], "ID")  #The first Header should be ID
 
-    def DLNewspaperReferences2025(self):
-        database = sqlite3.connect("database.db")
-        cursor = database.cursor()
-        cursor.execute("SELECT * FROM NewspaperReferences2025")
-        Data = cursor.fetchall()
-        Headers = [description[0] for description in cursor.description]
-        HeadersWithData = {}
-        for num in range(len(Headers) - 1):
-            HeadersWithData = {**HeadersWithData, Headers[num + 1]: [d[num + 1] for d in Data]}
-        df = pd.DataFrame(HeadersWithData)
-        writer = pd.ExcelWriter("NewspaperReferences2025.xlsx", engine="xlsxwriter")
-        df.to_excel(writer, sheet_name="sheet1", index=False)
-        workbook = writer.book
-        worksheet = writer.sheets["sheet1"]
-        worksheet.autofit()
-        workbook.close()
 
     def test_Data_Base_Header_Extraction_2(self):
         database = sqlite3.connect("database.db")
@@ -76,6 +60,8 @@ class MyTestCase(unittest.TestCase):
         cursor.execute("SELECT * FROM NewspaperReferences2025")
         Data = cursor.fetchall()
         self.assertEqual(Data[-1][0],  3629)  #the last data entry should be the 3629th value
+
+
 
 if __name__ == '__main__':
     unittest.main()
